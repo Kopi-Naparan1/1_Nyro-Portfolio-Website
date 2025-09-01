c47f12a1da6d66a3472549419ef1b4deb36388c9
unknown <Kopinaparan13@gmail.com>
2025-09-01 10:52:03 +0800

    Nyro-Portfolio-Website-Commit-1

---
+++ main.js
@@ -0,0 +1,21 @@
+let accordions = document.querySelectorAll(".accordion-wrapper .accordion");
+const nyroEmail = document.querySelector(".nyro-email");
+const emailForm = document.querySelector(".email-form-container");
+
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", () => {
+    // If already active, remove active and return
+    if (accordion.classList.contains("active")) {
+      accordion.classList.remove("active");
+    } else {
+      // Remove active from all
+      accordions.forEach((acc) => acc.classList.remove("active"));
+      // Add active to clicked
+      accordion.classList.add("active");
+    }
+  });
+});
+
+nyroEmail.addEventListener("click", () => {
+  emailForm.classList.toggle("active");
+});
